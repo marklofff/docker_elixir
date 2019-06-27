d772befd60284275684f7d06599794ed0c3263d1
Marcelo <qw3xvf@gmail.com>
2019-06-27 18:42:51 +0900

    omg..

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Docker.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :docker_elixir,
+    [app: :docker,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
